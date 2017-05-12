10436590715578ae1802bcf4f895c29fb8b34bbc
1824051 <1824051@00-46252.pusd.dom>
2017-05-12 15:09:17 -0700

    Cleaned up Game class code and comments

--- Pac-Man/src/Game.java
+++ Pac-Man/src/Game.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Game {
 
 	private Timer timer;
-	private final int TICK_LENGHT = 1000;
 	private Map map;
 	private UI ui;
 	private PacMan pacMan;
@@ -15,26 +14,24 @@ public class Game {
 	private int[] highScores;
 	private String[] highScorers;
 
-	public Game()
-	{
+	public Game() {
 		isPaused = true;//pauses game to prevent any grid action during grid setup
 		timer = new Timer();
-		timer.schedule(gameLoop(), TICK_LENGHT);
-		map = new Map();//the map class constructer is repsonsible for constructing all objects present on the map (ie. pacman, pacdots, walls, ghosts)
-		ui = new UI();//UI is reponsible for displaying a JFrame and interpreting the map to dipslay the game properly
-		pacMan = map.getPacMan();//TODO write get Pac Man method in map class to return contrusted Pac Man
-		shadow = map.getShadow();//TODO write get Shadow method like the get Pac Man method
-		speedy = map.getSpeedy();//TODO write get Speedy method like the get Pac Man method
-		bashful = map.getBashful();//TODO write get Bashful method like the get Pac Man method
-		pokey = map.getPokey();//TODO write get Pokey method like the get Pac Man method
+		timer.schedule(gameLoop(), 1000);
+		map = new Map();  // the map class constructor is responsible for constructing all objects present on the map (ie. pacman, pacdots, walls, ghosts)
+		ui = new UI();  // UI is responsible for displaying a JFrame and interpreting the map to dipslay the game properly
+		pacMan = map.getPacMan();  // TODO write get Pac Man method in map class to return constructed Pac Man
+		shadow = map.getShadow();  // TODO write get Shadow method like the getPacMan method
+		speedy = map.getSpeedy();  // TODO write get Speedy method like the getPacMan method
+		bashful = map.getBashful();  // TODO write get Bashful method like the getPacMan method
+		pokey = map.getPokey();  // TODO write getPokey method like the getPacMan method
 		highScores = new int[100];
 		highScorers = new String[100];
 		togglePause();
 		start();
 	}
 
-	public void start()
-	{
+	public void start() {
 		// starts or restarts the game
 		togglePause();//pauses game to prevent any grid action during grid setup
 		map = new Map();//reconstructs new map for new game, then reassigns all instance variables pointing to grid objects
@@ -46,8 +43,7 @@ public class Game {
 		togglePause();
 	}
 
-	public boolean togglePause()
-	{
+	public boolean togglePause() {
 		isPaused = !isPaused;
 		return isPaused;
 		// pauses or unpauses the game
@@ -57,22 +53,17 @@ public class Game {
 		return isPaused;
 	}
 
-	public TimerTask gameLoop() 
-	{
+	public TimerTask gameLoop() {
 		// performs tasks like updating the model and UI on set intervals (controlled by the Timer)
-		return new TimerTask()
-			{
-				@Override
-				public void run()
-				{
-					//input all periodic tasks per tick
-					if (isPaused)
-					{
-						ui.displayPaused();//TODO write method in UI class to display a paused screen if the method is called
-						return;
-					}
+		return new TimerTask() {
+			public void run() {
+				// input all periodic tasks per tick
+				if (isPaused) {
+					ui.displayPaused();  // TODO write method in UI class to display a "paused" screen if the method is called
+					return;
 				}
-			};
+			}
+		};
 	}
 
 }
