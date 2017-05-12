7937dc9ab925f977464dcfa8e3678583c9ea28ef
1824051 <1824051@users.noreply.github.com>
2017-05-12 00:46:56 -0700

    Added new methods from design plan

--- Pac-Man/src/Map.java
+++ Pac-Man/src/Map.java
@@ -10,6 +10,20 @@ public class Map {
 		return map[x][y];
 	}
 
+	public Object set(Object object, int x, int y) {
+		Object oldOccupant = map[x][y];
+		map[x][y] = object;
+		return oldOccupant;
+	}
+
+	public Object move(Object object, int x, int y) {
+		// moves the specified object to the specified new coordinates
+	}
+
+	public Object remove(Object object) {
+		// removes the specified object from the map model
+	}
+
 	public void reset() {
 		// returns all objects to the locations they occupy at the start of the game, and replaces any missing PacDots and PowerPellets
 	}
