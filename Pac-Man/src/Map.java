f098c3589dcd28a5bf6eb491dda1ed8412ae3ebe
1824051 <1824051@users.noreply.github.com>
2017-05-11 23:55:29 -0700

    Added new reset method from design plan

--- Pac-Man/src/Map.java
+++ Pac-Man/src/Map.java
@@ -1,11 +1,17 @@
+public class Map {
 
-public class Map 
-{
 	private Object[][] map;
-	
-	public Object getObjectAt(int x, int y)
-	{
-		
+
+	public Map() {
+		// ...
+	}
+
+	public Object getObjectAt(int x, int y) {
 		return map[x][y];
 	}
+
+	public void reset() {
+		// returns all objects to the locations they occupy at the start of the game, and replaces any missing PacDots and PowerPellets
+	}
+
 }
