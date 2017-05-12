72240ed93ac0a335d87c7c513ca151e8b1fbc850
1824051 <1824051@users.noreply.github.com>
2017-05-12 00:29:44 -0700

    Added new methods from design plan

--- Pac-Man/src/PacMan.java
+++ Pac-Man/src/PacMan.java
@@ -11,11 +11,23 @@ public class PacMan extends MoveableObject {
 	}
 
 	public Direction getQueuedDirection() {
-		return queuedDirection;    // we could use final constants rather than enums if we can't get this to compile
+		return queuedDirection;  // we could use final constants rather than enums if we can't get this to compile
 	}
 
 	public void setQueuedDirection(Direction newDirection) {
 		queuedDirection = newDirection;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void increaseScore(int points) {
+		score += points;
+	}
+
+	public void decreaseScore(int points) {
+		score -= points;
 	}
 
 }
