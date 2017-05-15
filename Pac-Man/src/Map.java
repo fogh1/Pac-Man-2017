8712e55c481bdcaa086a57bf615fbeeffdd9cc89
lolsykins123 <marklozovy@gmail.com>
2017-05-15 14:49:08 -0700

    added remove method to Map class. Might need some review.

--- Pac-Man/src/Map.java
+++ Pac-Man/src/Map.java
@@ -21,19 +21,30 @@ public class Map {
 		map[x][y] = object;
 		return oldOccupant;
 	}
-
+	
+	// moves the specified object to the specified new coordinates
 	public Object move(Object object, int x, int y) {
 		return null;  // temporary
-		// moves the specified object to the specified new coordinates
 	}
-
-	public Object remove(Object object) {
-		return null;  // temporary
-		// removes the specified object from the map model
+	
+	// removes the specified object from the map model
+	public void remove(Object object)
+	{
+		for (Object[] row : map)
+		{
+			for (Object tile : row)
+			{
+				if (tile == object)
+				{
+					tile = null;
+				}
+			}
+		}
 	}
-
+	
+	// returns all objects to the locations they occupy at the start of the game, and replaces any missing PacDots and PowerPellets
 	public void reset() {
-		// returns all objects to the locations they occupy at the start of the game, and replaces any missing PacDots and PowerPellets
+		
 	}
 
 	public PacMan getPacMan() {
