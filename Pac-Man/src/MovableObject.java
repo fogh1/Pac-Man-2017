578a9f6fe9f54bc4cdc6ad7166d36fe4976e9a88
1814171 <1814171@00-46241.pusd.dom>
2017-05-18 14:39:56 -0700

    Added a map instance variable, working on allowing moveable objects to check stuff from the map

--- Pac-Man/src/MovableObject.java
+++ Pac-Man/src/MovableObject.java
@@ -7,6 +7,7 @@ public abstract class MovableObject {
 	private int y;
 	private Direction direction;
 	private ImageIcon icon;
+	private Map currentMap;
 	
 	//TEMP
 	public MovableObject()
@@ -15,12 +16,13 @@ public abstract class MovableObject {
 	}
 	//TEMP
 
-	public MovableObject(int x, int y, Direction direction, String iconPath) {
+	public MovableObject(int x, int y, Direction direction, String iconPath, Map map) {
 		classLoader = this.getClass().getClassLoader();
 		this.x = x;
 		this.y = y;
 		this.direction = direction;
 		icon = new ImageIcon(classLoader.getResource(iconPath));
+		currentMap = map;
 	}
 
 	public int getX() {
@@ -30,6 +32,11 @@ public abstract class MovableObject {
 	public int getY() {
 		return y;
 	}
+	
+	public Map getMap()
+	{
+		return currentMap;
+	}
 
 	public void setX(int newX) {
 		x = newX;
@@ -37,6 +44,11 @@ public abstract class MovableObject {
 
 	public void setY(int newY) {
 		y = newY;
+	}
+	
+	public void setMap(Map newMap)
+	{
+		currentMap = newMap;
 	}
 
 	public Direction getDirection() {
