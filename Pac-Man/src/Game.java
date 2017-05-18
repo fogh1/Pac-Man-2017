98d61333bc9fc12911e616c4f6115b56e23c7ee7
1824051 <1824051@00-46252.pusd.dom>
2017-05-18 14:27:22 -0700

    Removed finished to-do items

--- Pac-Man/src/Game.java
+++ Pac-Man/src/Game.java
@@ -15,16 +15,16 @@ public class Game {
 	private String[] highScorers;
 
 	public Game() {
-		isPaused = true;//pauses game to prevent any grid action during grid setup
+		isPaused = true;  //pauses game to prevent any grid action during grid setup
 		timer = new Timer();
 		timer.scheduleAtFixedRate(gameLoop(), 500, 500);
 		map = new Map();  // the map class constructor is responsible for constructing all objects present on the map (ie. pacman, pacdots, walls, ghosts)
 		ui = new UI();  // UI is responsible for displaying a JFrame and interpreting the map to dipslay the game properly
-		pacMan = map.getPacMan();  // TODO write get Pac Man method in map class to return constructed Pac Man
-		shadow = map.getShadow();  // TODO write get Shadow method like the getPacMan method
-		speedy = map.getSpeedy();  // TODO write get Speedy method like the getPacMan method
-		bashful = map.getBashful();  // TODO write get Bashful method like the getPacMan method
-		pokey = map.getPokey();  // TODO write getPokey method like the getPacMan method
+		pacMan = map.getPacMan();
+		shadow = map.getShadow();
+		speedy = map.getSpeedy();
+		bashful = map.getBashful();
+		pokey = map.getPokey();
 		highScores = new int[100];
 		highScorers = new String[100];
 		togglePause();
@@ -33,8 +33,8 @@ public class Game {
 
 	public void start() {
 		// starts or restarts the game
-		togglePause();//pauses game to prevent any grid action during grid setup
-		map = new Map();//reconstructs new map for new game, then reassigns all instance variables pointing to grid objects
+		togglePause();  // pauses game to prevent any grid action during grid setup
+		map = new Map();  // reconstructs new map for new game, then reassigns all instance variables pointing to grid objects
 		pacMan = map.getPacMan();
 		shadow = map.getShadow();
 		speedy = map.getSpeedy();
