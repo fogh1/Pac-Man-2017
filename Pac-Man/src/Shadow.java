33826eabf7be614494262fe22d22ce973835eefa
1824051 <1824051@00-46252.pusd.dom>
2017-05-22 14:50:48 -0700

    Removed temporary method from Shadow class

--- Pac-Man/src/Shadow.java
+++ Pac-Man/src/Shadow.java
@@ -1,14 +1,5 @@
 public class Shadow extends Ghost {
 
-	public Shadow()
-	{
-		currentMode = GhostMode.CHASE;
-	}
-	
-	public void ChasePM(Object PacMan)
-	{
-		int[] dest = getMap().getAdjacentLocation(getMap().getPacMan());
-		getMap().move((getMap().getShadow()), dest[0], dest[1]);
-	}
+	// ...
 
 }
