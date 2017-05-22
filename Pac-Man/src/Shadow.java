09dbadf094268782ea83bbd182c589ee365471ed
lolsykins123 <marklozovy@gmail.com>
2017-05-22 14:46:41 -0700

    Added chase PacMan method to Shadow

--- Pac-Man/src/Shadow.java
+++ Pac-Man/src/Shadow.java
@@ -1,5 +1,14 @@
 public class Shadow extends Ghost {
 
-	// ...
+	public Shadow()
+	{
+		currentMode = GhostMode.CHASE;
+	}
+	
+	public void ChasePM(Object PacMan)
+	{
+		int[] dest = getMap().getAdjacentLocation(getMap().getPacMan());
+		getMap().move((getMap().getShadow()), dest[0], dest[1]);
+	}
 
 }
