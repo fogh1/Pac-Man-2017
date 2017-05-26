cf5a19b15aad3f423c5c57dd8b8f9d7ca8c87ffc
fogh1 <eliotfoss2@gmail.com>
2017-05-26 14:05:48 -0700

    Added basic AI to ghosts

--- Pac-Man/src/Shadow.java
+++ Pac-Man/src/Shadow.java
@@ -1,22 +1,44 @@
 public class Shadow extends Ghost {
 	
-	private int[] prevLoc;
 
 	public Shadow(int x, int y, Map map) {
 		super(x, y, map, "Shadow.png");
-		prevLoc = new int[2];
-		prevLoc[0] = x;
-		prevLoc[1] = y;
 		outsideRoom = true;
 	}
 
 	public void move() {
-		int[] prev = prevLoc;
-		prevLoc[0] = getX();
-		prevLoc[1] = getY();
 		if (atIntersection())
 		{
 			//TODO main AI for ghosts
+			double random = Math.random() * 2;
+			if (random < 1)
+			{
+				if (canMoveOn(getMap().getAdjacentObject(this)))
+				{
+					moveForward();
+				}
+				else
+				{
+					turn();
+					moveForward();
+				}
+			}
+			else
+			{
+				turn();
+				turn();
+				turn();
+				if (canMoveOn(getMap().getAdjacentObject(this)))
+				{
+					moveForward();
+				}
+				else
+				{
+					turn();
+					turn();
+					moveForward();
+				}
+			}
 		}
 		else if (atCorner())
 		{
--- Pac-Man/src/Speedy.java
+++ Pac-Man/src/Speedy.java
@@ -5,7 +5,57 @@ public class Speedy extends Ghost {
 	}
 
 	public void move() {
-		// ...
+		if (atIntersection())
+		{
+			//TODO main AI for ghosts
+			double random = Math.random() * 2;
+			if (random < 1)
+			{
+				if (canMoveOn(getMap().getAdjacentObject(this)))
+				{
+					moveForward();
+				}
+				else
+				{
+					turn();
+					moveForward();
+				}
+			}
+			else
+			{
+				turn();
+				turn();
+				turn();
+				if (canMoveOn(getMap().getAdjacentObject(this)))
+				{
+					moveForward();
+				}
+				else
+				{
+					turn();
+					turn();
+					moveForward();
+				}
+			}
+		}
+		else if (atCorner())
+		{
+			turn();
+			if (canMoveOn(getMap().getAdjacentObject(this)))
+			{
+				moveForward();
+			}
+			else
+			{
+				turn();
+				turn();
+				moveForward();
+			}
+		}
+		else
+		{
+			moveForward();
+		}
 	}
 
 }
