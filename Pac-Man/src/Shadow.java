46b9880eea963e64a4f98b059244d93ed35a0ba9
1824051 <1824051@users.noreply.github.com>
2017-05-31 18:08:17 -0700

    Refactored and restructured move code

--- Pac-Man/src/Shadow.java
+++ Pac-Man/src/Shadow.java
@@ -2,29 +2,13 @@ public class Shadow extends Ghost {
 	
 	public Shadow(int x, int y, Map map) {
 		super(x, y, map, "Shadow.png");
-		super.setIsOutsideRoom(true);
-		super.setMode(GhostMode.CHASE);
+		setOutsideRoom(true);
 	}
-	public void move()
-	{
-		if(getMode() == GhostMode.CHASE)
-		{
-			chaseMove();
-		}
-		//if(getMode() == GhostMode.SCATTER)
-		//{
-		//	scatterMove();
-		//}
-		else
-		{
-			super.move();
-		}
-	}
-	
-	public void chaseMove()
-	{
+
+	public void move() {
+		if (getMode() == GhostMode.CHASE) {
 			if (isAtIntersection()) {
-				Direction newDirection = determineNewDirection(getMap().getPacMan().getX(), getMap().getPacMan().getY()); //wants to get to PacMan
+				Direction newDirection = getDirectionTowardsTarget(getMap().getPacMan());
 				setDirection(newDirection);
 				moveForward();
 			}
@@ -45,6 +29,10 @@ public class Shadow extends Ghost {
 					}
 				}
 			}
+		}
+		else {
+			super.move();
+		}
 	}
 
 }
