4d723df5fab811f10bae9297474c12aaaf8d1961
1814171 <1814171@00-46241.pusd.dom>
2017-06-02 14:23:03 -0700

    modified Speedy chasMove()

--- Pac-Man/src/Speedy.java
+++ Pac-Man/src/Speedy.java
@@ -16,5 +16,51 @@ public class Speedy extends Ghost {
 			super.move();
 		}
 	}
+	
+	public void chaseMove()
+	{
+		PacMan p = getMap().getPacMan();
+		int x = p.getX();
+		int y = p.getY();
+		Direction pDirection = p.getDirection();
+		if(pDirection == Direction.LEFT && x >= 4)
+		{
+			x=x-4;
+		}
+		else if(pDirection == Direction.RIGHT && x < 24)
+		{
+			x=x+4;
+		}
+		else if(pDirection == Direction.DOWN && y < 24)
+		{
+			y = y+4;
+		}
+		else if(pDirection == Direction.UP && y >= 4)
+		{
+			y = y-4;
+		}
+		if (isAtIntersection()) {
+			Direction newDirection = getDirectionTowardsTarget(x, y); //wants to get to PacMan
+			setDirection(newDirection);
+			moveForward();
+		}
+		else if (isAtCorner()) {
+			turn();
+			if (!moveForward()) {
+				turn();
+				turn();
+				moveForward();
+			}
+		}
+		else {
+			if (!moveForward()) {
+				turn();
+				if (!moveForward()) {
+					turn();
+					moveForward();
+				}
+			}
+		}
+	}
 
 }
